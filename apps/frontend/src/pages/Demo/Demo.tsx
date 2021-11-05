59d31e9ce6163d04b374f9db9500bfbd2485f420
Konstantin Spieß <konstantinspiess@outlook.de>
2021-11-05 18:45:10 +0100

    set up auth package with initial firebase component

--- apps/frontend/src/pages/Demo/Demo.tsx
+++ apps/frontend/src/pages/Demo/Demo.tsx
@@ -1,18 +1,24 @@
 import TaskList from "@vc/frontend/component/TaskList/TaskList";
+import React from "react";
+import { FirebaseUI } from "@vc/auth";
 
 export default function Demo() {
 	return (
-		<div
-			style={{
-				height: "100%",
-				display: "flex",
-				alignItems: "center",
-				justifyContent: "center",
-				background:
-					"radial-gradient(95.33% 88.77% at 100% 25.99%, #F3EAE4 0%, #F5EBDF 11.12%, #F6F5F8 33.47%)",
-			}}
-		>
-			<TaskList />
-		</div>
+		<React.Fragment>
+			<div
+				style={{
+					height: "100%",
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					justifyContent: "center",
+					background:
+						"radial-gradient(95.33% 88.77% at 100% 25.99%, #F3EAE4 0%, #F5EBDF 11.12%, #F6F5F8 33.47%)",
+				}}
+			>
+				<FirebaseUI />
+				<TaskList />
+			</div>
+		</React.Fragment>
 	);
 }
---
+++ packages/auth/src/FirebaseUI.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from "react";
+
+export const FirebaseUI: FC = () => {
+	return <div>FIREBASE LOGIN</div>;
+};
