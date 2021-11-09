94fb3b93cbf20eb865b7b88b77e9e77ecbb2f614
Konstantin Spieß <konstantinspiess@outlook.de>
2021-11-09 17:32:25 +0100

    fixed env type declaration

--- packages/auth/src/env.d.ts
+++ packages/auth/src/env.d.ts
@@ -1,10 +1,10 @@
 interface ImportMetaEnv extends Readonly<Record<string, string>> {
-	readonly REACT_APP_FIREBASE_API_KEY: string;
-	readonly REACT_APP_FIREBASE_AUTH_DOMAIN: string;
-	readonly REACT_APP_FIREBASE_PROJECT_ID: string;
-	readonly REACT_APP_FIREBASE_STORAGE_BUCKET: string;
-	readonly REACT_APP_FIREBASE_MESSAGING_SENDER_ID: string;
-	readonly REACT_APP_FIREBASE_APP_ID: string;
+	readonly VITE_FIREBASE_API_KEY: string;
+	readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+	readonly VITE_FIREBASE_PROJECT_ID: string;
+	readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+	readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+	readonly VITE_FIREBASE_APP_ID: string;
 }
 
 interface ImportMeta {
