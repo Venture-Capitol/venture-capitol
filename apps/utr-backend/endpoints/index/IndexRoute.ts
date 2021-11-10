dba689899506438dc85d68ddf42ae91754dd4565
eloluap <p.saalfeld@flohr-cie.de>
2021-11-10 09:58:27 +0100

    added authentication in backend

--- apps/utr-backend/endpoints/index/IndexRoute.ts
+++ apps/utr-backend/endpoints/index/IndexRoute.ts
@@ -1,9 +1,10 @@
 import express = require("express");
 import { Router } from "express";
 const router = Router();
+import { isAuthenticated } from "../utils/AuthenticationUtils";
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", isAuthenticated, function (req, res, next) {
 	/* res.render('index', { title: 'Express' }); */
 	res.status(200).end();
 });
