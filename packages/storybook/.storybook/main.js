ce7ee4523f6c3a7c1c348dbdb33087d80c878057
Luis Rieke <66038170+luis-rieke@users.noreply.github.com>
2021-11-17 13:34:05 +0100

    Boyscout/storybook (#19)
    * logo video fix
    
    * fix package-lock.json files
    
    Co-authored-by: Konstantin Spieß <konstantinspiess@outlook.de>

---
+++ packages/storybook/.storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+	stories: [
+		// TODO: make all stories visible at the same time
+
+		// --- GPF components ---
+		"../../../apps/frontend/src/components/**/*.stories.mdx",
+		"../../../apps/frontend/src/components/**/*.stories.@(js|jsx|ts|tsx)",
+
+		// --- UTR components ---
+		"../../../apps/utr-frontend/components/**/*.stories.mdx",
+		"../../../apps/utr-frontend/components/**/*.stories.@(js|jsx|ts|tsx)",
+
+		// --- Global components ---
+		"../../ui/**/*.stories.mdx",
+		"../../ui/**/*.stories.@(js|jsx|ts|tsx)",
+	],
+	addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
