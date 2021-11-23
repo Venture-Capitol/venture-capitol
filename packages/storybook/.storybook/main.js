a0a3698650997f1617b80993988ab499ca21b65c
luis-rieke <luis.rieke@europace.de>
2021-11-24 00:17:53 +0100

    storybook speedup

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,4 +1,14 @@
 module.exports = {
+	typescript: {
+		check: false,
+		checkOptions: {},
+		reactDocgen: "react-docgen-typescript",
+		reactDocgenTypescriptOptions: {
+			shouldExtractLiteralValuesFromEnum: true,
+			propFilter: prop =>
+				prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+		},
+	},
 	stories: [
 		// TODO: make all stories visible at the same time
 
