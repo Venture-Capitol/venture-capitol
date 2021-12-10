08c5e4acab409e6fbc544a27b81ec2a8991fe210
Malte <hi@malts.me>
2021-12-10 18:02:09 +0100

    [GPF-66] Initialize CI/CD deployment (#28)
    earth_africa Added Terraform for infrastructure configuration
    microscope Added Github action to perform terraform verification on pull requests
    package Added Github action to verify docker build on pull requests
    truck Added Github action to push docker to Google Artifact Registry
    cloud Added Github action to apply terraform config on push on master

---
+++ deploy/init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Get the project id, for this to worky you have to log in with gcloud cli first
+PROJECT_ID=$(gcloud config get-value project)
+# Create a new storage bucket, enable versioning and restrict external access
+gsutil mb -l eur4 gs://${PROJECT_ID}-tfstate
+gsutil versioning set on gs://${PROJECT_ID}-tfstate
+gsutil pap set enforced gs://${PROJECT_ID}-tfstate
+# Versioning keeps old versions of a file around, to limit storage costs, files with more than 3 new versions get deleted automatically
+# We create a temporary file containing this policy
+echo '{"rule": [{"action": {"type": "Delete"}, "condition": {"isLive": false, "numNewerVersions": 3}}]}' > tfStateLifecycleRule.json
+gsutil lifecycle set ./tfStateLifecycleRule.json gs://${PROJECT_ID}-tfstate
+rm ./tfStateLifecycleRule.json
+# Set terraform variable project id
+# echo "project = \"${PROJECT_ID}\"" > terraform.tfvars
+# Create an Artifact Registry for Docker images
+gcloud artifacts repositories create images \
+    --repository-format=DOCKER \
+    --location=europe-west1 \
+    --description="Docker image repository for all venture capitol apps" \
+
