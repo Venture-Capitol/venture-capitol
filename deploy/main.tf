f0c41782d0eadf75373f73fe896097af77f19c4b
Malte <skyguardian42@gmail.com>
2021-12-14 01:03:03 +0100

    add domain mapping to terraform

--- deploy/main.tf
+++ deploy/main.tf
@@ -188,3 +188,17 @@ resource "google_cloud_run_service_iam_policy" "noauth" {
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
+
+
+resource "google_cloud_run_domain_mapping" "staging" {
+  location = var.region
+  name     = "api.venturecapitol.de"
+
+  metadata {
+    namespace = var.project
+  }
+
+  spec {
+    route_name = google_cloud_run_service.backend.name
+  }
+}
--- deploy/staging.tf
+++ deploy/staging.tf
@@ -78,3 +78,16 @@ resource "google_cloud_run_service" "staging_backend" {
 
   depends_on = [google_secret_manager_secret_version.staging_db_connection_string, google_project_service.run]
 }
+
+resource "google_cloud_run_domain_mapping" "staging" {
+  location = var.region
+  name     = "staging.api.venturecapitol.de"
+
+  metadata {
+    namespace = var.project
+  }
+
+  spec {
+    route_name = google_cloud_run_service.staging_backend.name
+  }
+}
