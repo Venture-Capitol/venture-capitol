bab4ea93a9a6e81c017e86e29207bad1f49d1f63
Malte <skyguardian42@gmail.com>
2021-12-17 00:10:42 +0100

    format terraform

--- deploy/frontend.tf
+++ deploy/frontend.tf
@@ -5,15 +5,15 @@ resource "google_storage_bucket_iam" "allow" {
 }
 
 resource "google_storage_bucket" "frontend_staging" {
-  name = "vc-frontend-staging"
-  location = var.region
-  force_destroy = true
+  name                        = "vc-frontend-staging"
+  location                    = var.region
+  force_destroy               = true
   uniform_bucket_level_access = true
-  
+
 
   website {
     main_page_suffix = "index.html"
-    not_found_page = "index.html"
+    not_found_page   = "index.html"
   }
 }
 
