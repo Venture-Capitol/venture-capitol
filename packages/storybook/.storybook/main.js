cb63e79cb4cb63f97e8259254ac87a164ad2cae6
Julian Hoff <julian@jut-so.de>
2021-12-19 17:24:40 +0100

    sass issue resolved, working on import path issue now

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,9 +1,18 @@
 const path = require("path");
 
 module.exports = {
-	stories: [
-		// TODO: make all stories visible at the same time
+	typescript: {
+		check: false,
+		checkOptions: {},
+		reactDocgen: "react-docgen-typescript",
+		reactDocgenTypescriptOptions: {
+			shouldExtractLiteralValuesFromEnum: true,
+			propFilter: prop =>
+				prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+		},
+	},
 
+	stories: [
 		// --- GPF components ---
 		"../../../apps/frontend/src/components/**/*.stories.mdx",
 		"../../../apps/frontend/src/components/**/*.stories.@(js|jsx|ts|tsx)",
@@ -19,11 +28,14 @@ module.exports = {
 		"@storybook/preset-scss",
 	],
 
-	webpackFinal: async config => {
-		// add SCSS support for CSS Modules
+	webpackFinal: async (config, { configType }) => {
+		// `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+		// You can change the configuration based on that.
+		// 'PRODUCTION' is used when building the static version of storybook.
+
 		config.module.rules.push({
 			test: /\.scss$/,
-			use: ["style-loader", "css-loader?modules&importLoaders", "sass-loader"],
+			use: ["style-loader", "css-loader", "sass-loader"],
 			include: path.resolve(__dirname, "../"),
 		});
 
