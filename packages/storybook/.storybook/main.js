2b631d847aa3f80bd066e822122ca10c0d701c08
Julian Hoff <julian@jut-so.de>
2021-12-19 14:18:15 +0100

    before webpack html update

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
 	stories: [
 		// TODO: make all stories visible at the same time
@@ -10,5 +12,21 @@ module.exports = {
 		"../../ui/**/*.stories.mdx",
 		"../../ui/**/*.stories.@(js|jsx|ts|tsx)",
 	],
-	addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/preset-scss",
+	],
+
+	webpackFinal: async config => {
+		// add SCSS support for CSS Modules
+		config.module.rules.push({
+			test: /\.scss$/,
+			use: ["style-loader", "css-loader?modules&importLoaders", "sass-loader"],
+			include: path.resolve(__dirname, "../"),
+		});
+
+		return config;
+	},
 };
