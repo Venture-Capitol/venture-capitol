401b703cf3d21d22912a43c7e15a23012794c229
Julian Hoff <julian@jut-so.de>
2021-12-27 12:52:10 +0100

    transferred the sentry changes to a new branch from main

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import "./global.scss";
 import App from "./App";
+
+if (import.meta.env.PROD) {
+	Sentry.init({
+		dsn: "https://4a6047aa227744c3a069f323ff2dc54a@o1053443.ingest.sentry.io/6037959",
+		integrations: [new Integrations.BrowserTracing()],
+
+		// Set tracesSampleRate to 1.0 to capture 100%
+		// of transactions for performance monitoring.
+		// We recommend adjusting this value in production
+		tracesSampleRate: 1.0,
+	});
+}
 
 ReactDOM.render(
 	<React.StrictMode>
