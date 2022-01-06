3f4a2516b45041d61f6f567e21e6a96d4d28e0cc
Malte <skyguardian42@gmail.com>
2022-01-06 17:51:26 +0100

    added localstorage demo

--- apps/frontend/src/pages/Demo/Demo.tsx
+++ apps/frontend/src/pages/Demo/Demo.tsx
@@ -1,7 +1,25 @@
 import TaskList from "@vc/frontend/component/TaskList/TaskList";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Demo() {
+	let [text, setText] = useState("");
+
+	function updateFromStorage() {
+		let text = window.localStorage.getItem("text");
+		setText(text || "");
+	}
+
+	useEffect(() => updateFromStorage(), []);
+
+	useEffect(() => {
+		let l = window.addEventListener("storage", updateFromStorage);
+		return () => window.removeEventListener("storage", updateFromStorage);
+	});
+
+	useEffect(() => {
+		window.localStorage.setItem("text", text);
+	}, [text]);
+
 	return (
 		<div
 			style={{
@@ -14,7 +32,16 @@ export default function Demo() {
 					"radial-gradient(95.33% 88.77% at 100% 25.99%, #F3EAE4 0%, #F5EBDF 11.12%, #F6F5F8 33.47%)",
 			}}
 		>
-			<TaskList />
+			<h1>Hello</h1>
+			{/* synchronize input with t  */}
+			<input
+				type='text'
+				placeholder={"schreib was..."}
+				value={text}
+				onChange={e => setText(e.target.value)}
+			/>
+
+			{/* <TaskList /> */}
 		</div>
 	);
 }
