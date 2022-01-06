c6557096fe6c988babd5d87ca7ce54c25172337c
eloluap <p.saalfeld@flohr-cie.de>
2022-01-06 18:16:41 +0100

    changed getRole

--- apps/utr-backend/src/endpoints/utils/AuthenticationUtils.ts
+++ apps/utr-backend/src/endpoints/utils/AuthenticationUtils.ts
@@ -34,34 +34,31 @@ export async function isAuthenticatedAsAdmin(req: any, res: any, next: any) {
 	}
 }
 
-export async function getRole(req: any, res: any, next: any) {
-	if (typeof req.headers.authorization !== "undefined") {
+export function getRole(req: any, res: any, next: any) {
+	if (req.headers.authorization !== undefined) {
 		const token = req.headers.authorization.split(" ")[1];
 		try {
 			// GET ROLE -> save in req.role
-			await getAuth()
+			getAuth()
 				.verifyIdToken(token)
-				.then((claims: any) => {
-					if (
-						claims != undefined &&
-						claims.isAdmin != undefined &&
-						claims.isAdmin == true
-					) {
-						req.role = "admin";
-					} else {
-						req.role = "user";
+				.then(
+					tokenResult => {
+						if (tokenResult.role == undefined) {
+							// SET Custom Claim
+							tokenResult.role = "user";
+						}
+						req.user = tokenResult;
+						return next();
+					},
+					() => {
+						res.status(401).end();
 					}
-				});
-			if (req.role == "admin" || req.role == "user") {
-				return next();
-			} else {
-				res.status(401).end();
-			}
+				);
 		} catch (e) {
 			res.status(500).end();
 		}
 	} else {
-		req.role = "user";
-		return next();
+		res.status(401).end();
+		return;
 	}
 }
