b958b6866d2b0726ec7a13601b838827f8cc65e0
GLUONSTUDIUM\kk <kk>
2022-01-06 18:14:23 +0100

    error instanceof differentation, changed type any

--- apps/utr-backend/src/endpoints/entry/EntryRoute.ts
+++ apps/utr-backend/src/endpoints/entry/EntryRoute.ts
@@ -3,8 +3,10 @@ const router = Router();
 import { isAuthenticated } from "../utils/AuthenticationUtils";
 
 import logger = require("../../config/winston");
+
 import { Entry } from "@prisma/client";
 import ApplicationError from "../utils/ApplicationError";
+
 const EntryService = require("./EntryService");
 const EntryUtils = require("../utils/EntryUtils");
 
@@ -24,28 +26,33 @@ router.get("/search", function (req, res, next) {
 					res.status(500).end(error.message);
 				}
 			} else {
-				for (const entry in result) {
+				/*for (const entry in result) {
 					// Array.map -> führt für jedes Element aus
 					const { id, job, company, address, description, ...partialObject } =
 						result[entry];
 					result[entry] = { id, job, company, address, description };
 				}
+				*/
+				const subsetResult = result.map(function () {});
 				res.send(result);
 			}
 		}
 	);
 });
 
-// add isAuthenticated when ready
 router.get("/", function (req, res, next) {
 	const verifiedAsBoolean = EntryUtils.parseToBoolean(req.query.verified);
 	const amountAsNumber = EntryUtils.parseToNumber(req.query.amount);
 	const pageAsNumber = EntryUtils.parseToNumber(req.query.page);
 	EntryService.getAllEntries(
-		function (error: any, result: any) {
+		function (error: Error | ApplicationError, result: Entry[]) {
 			if (error) {
 				logger.error(error.message);
-				res.status(error.errorCode).end(error.message);
+				if (error instanceof ApplicationError) {
+					res.status(error.errorCode).end(error.message);
+				} else {
+					res.status(500).end(error.message);
+				}
 			} else if (result) {
 				res.send(result);
 			}
@@ -65,10 +72,14 @@ router.post("/", function (req, res, next) {
 		body.latitude,
 		body.longitude,
 		body.email,
-		function (error: any, result: any) {
+		function (error: Error | ApplicationError, result: Entry) {
 			if (error) {
 				logger.error(error.message);
-				res.status(error.errorCode).end(error.message);
+				if (error instanceof ApplicationError) {
+					res.status(error.errorCode).end(error.message);
+				} else {
+					res.status(500).end(error.message);
+				}
 			} else if (result) {
 				res.status(200).end();
 			}
@@ -81,54 +92,73 @@ router.post("/", function (req, res, next) {
 
 router.get("/:id", function (req, res, next) {
 	const idAsNumber = Number(req.params.id);
-	EntryService.getEntry(idAsNumber, function (error: any, result: any) {
-		if (error) {
-			logger.error(error.message);
-			res.status(error.errorCode).end(logger.error);
-		} else if (result) {
-			console.log(result);
-			console.log(typeof result);
-			res.send(result);
-		}
-	});
-});
-
-// add isAuthenticated when ready
-router.put("/:id", function (req, res, next) {
-	const idAsNumber = EntryUtils.parseToNumber(req.params.id);
-	EntryService.updateEntry(
+	EntryService.getEntry(
 		idAsNumber,
-		req.body.editedEntry,
-		function (error: any, result: any) {
+		function (error: Error | ApplicationError, result: Entry) {
 			if (error) {
 				logger.error(error.message);
-				res.status(error.errorCode).end(error.message);
+				if (error instanceof ApplicationError) {
+					res.status(error.errorCode).end(error.message);
+				} else {
+					res.status(500).end(error.message);
+				}
 			} else if (result) {
+				console.log(result);
+				console.log(typeof result);
 				res.send(result);
 			}
 		}
 	);
 });
 
-// add isAuthenticated when ready
-router.delete("/:id", function (req, res, next) {
+router.put("/:id", function (req, res, next) {
 	const idAsNumber = EntryUtils.parseToNumber(req.params.id);
-	EntryService.deleteEntry(idAsNumber, function (error: any) {
-		if (error) {
-			logger.error(error.message);
-			res.status(error.errorCode).end(error.message);
-		} else {
-			res.send("Eintrag mit ID: " + req.params.id + " geloescht.");
+	EntryService.updateEntry(
+		idAsNumber,
+		req.body.editedEntry,
+		function (error: Error | ApplicationError, result: Entry) {
+			if (error) {
+				logger.error(error.message);
+				if (error instanceof ApplicationError) {
+					res.status(error.errorCode).end(error.message);
+				} else {
+					res.status(500).end(error.message);
+				}
+			} else if (result) {
+				res.status(200).end();
+			}
 		}
-	});
+	);
 });
 
-// could add isAuthenticated when ready, but will be deleted before publish anyways
+router.delete("/:id", function (req, res, next) {
+	const idAsNumber = EntryUtils.parseToNumber(req.params.id);
+	EntryService.deleteEntry(
+		idAsNumber,
+		function (error: Error | ApplicationError) {
+			if (error) {
+				logger.error(error.message);
+				if (error instanceof ApplicationError) {
+					res.status(error.errorCode).end(error.message);
+				} else {
+					res.status(500).end(error.message);
+				}
+			} else {
+				res.send("Eintrag mit ID: " + req.params.id + " geloescht.");
+			}
+		}
+	);
+});
+
 router.post("/addMany", function (req, res, next) {
-	EntryUtils.addManyEntries(function (error: any) {
+	EntryUtils.addManyEntries(function (error: Error | ApplicationError) {
 		if (error) {
 			logger.error(error.message);
-			res.status(error.errorCode).end(error.message);
+			if (error instanceof ApplicationError) {
+				res.status(error.errorCode).end(error.message);
+			} else {
+				res.status(500).end(error.message);
+			}
 		} else {
 			res.status(200).end();
 		}
