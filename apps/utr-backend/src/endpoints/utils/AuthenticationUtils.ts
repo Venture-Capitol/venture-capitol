8fecaeac01529c4b8c4a4a1342563301d9a8a213
eloluap <p.saalfeld@flohr-cie.de>
2022-01-09 18:37:42 +0100

    rewrite with try catch instead of then
    Co-authored-by: Malte <SkyGuardian42@users.noreply.github.com>

--- apps/utr-backend/src/endpoints/utils/AuthenticationUtils.ts
+++ apps/utr-backend/src/endpoints/utils/AuthenticationUtils.ts
@@ -3,51 +3,43 @@ import { getAuth } from "firebase-admin/auth";
 import logger = require("../../config/winston");
 
 // Checks if the user is logged in and saves the user object in req.user
-export function getUser(req: Request, res: Response, next: NextFunction) {
-	if (req.headers.authorization !== undefined) {
-		const token = req.headers.authorization.split(" ")[1];
-		try {
-			getAuth()
-				.verifyIdToken(token)
-				.then(
-					tokenResult => {
-						// set Custom Claim role as user if custom claim is not set
-						if (tokenResult.role == undefined) {
-							getAuth()
-								.setCustomUserClaims(tokenResult.uid, { role: "user" })
-								.then(() => {
-									logger.debug(
-										"Custom Claim role: 'user' successfully added to UID: " +
-											tokenResult.uid
-									);
-								})
-								.catch(error => {
-									logger.error(
-										"Error on user with UID: " +
-											tokenResult.uid +
-											" while adding Custom Claim role: 'user': ",
-										error
-									);
-								});
-							tokenResult.role = "user";
-						}
-						// save the user object from firebase into the request
-						req.user = tokenResult;
-						return next();
-					},
-					// if token could not be verified return 401
-					() => {
-						res.status(401).end();
-					}
-				);
-		} catch (e) {
-			// if an exception occurs return 401
-			res.status(500).end();
-		}
-	} else {
-		// if user is not logged in return 401
-		res.status(401).end();
+export async function getUser(req: Request, res: Response, next: NextFunction) {
+	// if user is not logged in return 401
+	if (req.headers.authorization == undefined) {
+		return res.status(401).end();
 	}
+
+	const token = req.headers.authorization.split(" ")[1];
+
+	let tokenResult;
+
+	try {
+		tokenResult = await getAuth().verifyIdToken(token);
+	} catch {
+		return res.status(401).end();
+	}
+
+	try {
+		if (tokenResult.role == undefined) {
+			tokenResult.role = "user";
+			await getAuth().setCustomUserClaims(tokenResult.uid, { role: "user" });
+			logger.debug(
+				"Custom Claim role: 'user' successfully added to UID: " +
+					tokenResult.uid
+			);
+		}
+	} catch (e) {
+		logger.error(
+			"Error on user with UID: " +
+				tokenResult.uid +
+				" while adding Custom Claim role: 'user': ",
+			e
+		);
+	}
+
+	// save the user object from firebase into the request
+	req.user = tokenResult;
+	return next();
 }
 
 // Checks if the user is an admin - Can ONLY be used after getUser was used
