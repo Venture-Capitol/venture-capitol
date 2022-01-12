8dc1b1780ba274cb2b4f4edd54b47074b0a2a0ab
Malte <skyguardian42@gmail.com>
2022-01-13 00:30:52 +0100

    restyle

--- apps/frontend/src/components/TaskList/TaskNode/TaskNode.tsx
+++ apps/frontend/src/components/TaskList/TaskNode/TaskNode.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useState, MouseEvent } from "react";
+import { FunctionComponent, useState, MouseEvent, useMemo } from "react";
 import styles from "./TaskNode.module.scss";
 import checkmarkIcon from "../../../assets/checkmark.svg";
 import { useHistory } from "react-router-dom";
@@ -67,3 +67,55 @@ const Task: FunctionComponent<TaskProps> = ({
 };
 
 export default Task;
+
+interface TaskNodeContainerProps {
+	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
+	checked: boolean;
+	id?: string;
+	text: string;
+	clickEverywhere?: boolean;
+}
+export function TaskNodeContainer({
+	onChange,
+	onClick,
+	checked,
+	id,
+	text,
+}: TaskNodeContainerProps) {
+	const [inputId] = useMemo(() => [Math.random().toString()], []);
+
+	return (
+		<div
+			className={`${styles.task} box`}
+			data-checked={checked}
+			data-id={id}
+			onClick={onClick}
+		>
+			<input
+				checked={checked}
+				onChange={onChange}
+				type='checkbox'
+				name='task_checked'
+				onClick={e => e.stopPropagation()}
+				id={inputId}
+			/>
+			<label htmlFor={inputId} onClick={e => e.stopPropagation()}>
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					fill='none'
+					viewBox='0 0 24 24'
+					stroke='currentColor'
+				>
+					<path
+						strokeLinecap='round'
+						strokeLinejoin='round'
+						strokeWidth='2'
+						d='M5 13l4 4L19 7'
+					/>
+				</svg>{" "}
+			</label>
+			<span>{text}</span>
+		</div>
+	);
+}
--- apps/frontend/src/pages/Gruendung/Gruendung.tsx
+++ apps/frontend/src/pages/Gruendung/Gruendung.tsx
@@ -1,5 +1,10 @@
+import { TaskNodeContainer } from "@vc/frontend/component/TaskList/TaskNode/TaskNode";
 import Button from "@vc/ui/src/components/Button/Button";
-import { useGruendungContext } from "contexts/Gruendung/Gruendung";
+import {
+	ProcessedTaskNode,
+	ProcessedTaskNodes,
+	useGruendungContext,
+} from "contexts/Gruendung/Gruendung";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import TaskList from "../../components/TaskList/TaskList";
@@ -72,72 +77,171 @@ const Gruendung = () => {
 					<div dangerouslySetInnerHTML={{ __html: htmlContent }} />
 				)}
 
-				{/* Show button to proceed for tasks */}
-				{task &&
-					unprocessedNodes[task] &&
-					unprocessedNodes[task].type == "task" &&
-					unprocessedNodes[task].next.map(next => {
-						return (
-							<Link to={next} key={unprocessedNodes[next].shortName}>
-								<Button>Weiter zu {unprocessedNodes[next].shortName}</Button>
-							</Link>
-						);
-					})}
-
 				{/* Show decision options for decision */}
 				{task &&
 					unprocessedNodes[task] &&
 					unprocessedNodes[task].type == "decision" && (
-						<>
-							<div className={s.aside}>
-								<Toggle
-									checked={nodes[task]?.selectedPath == 0}
-									onChange={active =>
-										setDecisionStatus(task, active ? 0 : undefined)
-									}
-									textUnchecked={
-										unprocessedNodes[unprocessedNodes[task].next[0]].shortName +
-										" wählen"
-									}
-									textChecked={
-										unprocessedNodes[unprocessedNodes[task].next[0]].shortName +
-										" gewählt"
-									}
-								/>
-								<Toggle
-									checked={nodes[task]?.selectedPath == 1}
-									onChange={active =>
-										setDecisionStatus(task, active ? 1 : undefined)
-									}
-									textUnchecked={
-										unprocessedNodes[unprocessedNodes[task].next[1]].shortName +
-										" wählen"
-									}
-									textChecked={
-										unprocessedNodes[unprocessedNodes[task].next[1]].shortName +
-										" gewählt"
-									}
-								/>
-							</div>
-							{nodes[task]?.selectedPath != undefined ? (
-								<Link to={task}>
-									<Button>
-										Weiter zu{" "}
-										{
-											unprocessedNodes[
-												unprocessedNodes[task].next[nodes[task].selectedPath!]
-											].shortName
-										}
-									</Button>
-								</Link>
-							) : (
-								<Button disabled={true}>Erst eine Auswahl treffen</Button>
-							)}
-						</>
+						<div className={s.aside}>
+							<TaskNodeContainer
+								checked={nodes[task]?.selectedPath == 0}
+								onChange={active =>
+									setDecisionStatus(
+										task,
+										nodes[task]?.selectedPath == 0 ? undefined : 0
+									)
+								}
+								onClick={() => {
+									setDecisionStatus(
+										task,
+										nodes[task]?.selectedPath == 0 ? undefined : 0
+									);
+								}}
+								text={
+									nodes[task]?.selectedPath == 0
+										? unprocessedNodes[unprocessedNodes[task].next[0]]
+												.shortName + " gewählt"
+										: unprocessedNodes[unprocessedNodes[task].next[0]]
+												.shortName + " wählen"
+								}
+							/>
+							<TaskNodeContainer
+								checked={nodes[task]?.selectedPath == 1}
+								onChange={active => {
+									console.log(active);
+									setDecisionStatus(
+										task,
+										nodes[task]?.selectedPath == 1 ? undefined : 1
+									);
+								}}
+								onClick={() => {
+									setDecisionStatus(
+										task,
+										nodes[task]?.selectedPath == 1 ? undefined : 1
+									);
+								}}
+								text={
+									nodes[task]?.selectedPath == 1
+										? unprocessedNodes[unprocessedNodes[task].next[1]]
+												.shortName + " gewählt"
+										: unprocessedNodes[unprocessedNodes[task].next[1]]
+												.shortName + " wählen"
+								}
+							/>
+						</div>
 					)}
+
+				{/* back / forward navigation */}
+				<div className={s.bottomNav}>
+					{/* Go back */}
+					{nodes[task]?.prev[0] && (
+						<Link to={nodes[task].prev[0]} key={nodes[task].prev[0]}>
+							<svg
+								width='15'
+								height='15'
+								viewBox='0 0 15 15'
+								fill='none'
+								transform='scale(-1 1)'
+								xmlns='http://www.w3.org/2000/svg'
+							>
+								<path
+									d='M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z'
+									fill='currentColor'
+									fillRule='evenodd'
+									clipRule='evenodd'
+								></path>
+							</svg>
+							Zurück zu {nodes[nodes[task].prev[0]].shortName}
+						</Link>
+					)}
+
+					{/* if not decision, go forward */}
+					{nodes[task]?.type != "decision" && nodes[task]?.next[0] && (
+						<Link
+							to={unprocessedNodes[task].next[0]}
+							key={unprocessedNodes[task].next[0]}
+						>
+							Weiter zu{" "}
+							{unprocessedNodes[unprocessedNodes[task].next[0]].shortName}
+							<svg
+								width='15'
+								height='15'
+								viewBox='0 0 15 15'
+								fill='none'
+								xmlns='http://www.w3.org/2000/svg'
+							>
+								<path
+									d='M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z'
+									fill='currentColor'
+									fillRule='evenodd'
+									clipRule='evenodd'
+								></path>
+							</svg>
+						</Link>
+					)}
+
+					{/* if decision && selctedPath ? next : selectsomething */}
+					{nodes[task]?.type == "decision" ? (
+						nodes[task]?.selectedPath != undefined ? (
+							<Link to={unprocessedNodes[task].next[nodes[task].selectedPath!]}>
+								Weiter zu{" "}
+								{/* {console.log(  nodes[task].next[nodes[task].selectedPath!])} */}
+								{/* {console.log(unprocessedNodes[task].next[nodes[task].selectedPath!])} */}
+								{
+									unprocessedNodes[
+										unprocessedNodes[task].next[nodes[task].selectedPath!]
+									]?.shortName
+								}
+								<svg
+									width='15'
+									height='15'
+									viewBox='0 0 15 15'
+									fill='none'
+									xmlns='http://www.w3.org/2000/svg'
+								>
+									<path
+										d='M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z'
+										fill='currentColor'
+										fillRule='evenodd'
+										clipRule='evenodd'
+									></path>
+								</svg>
+							</Link>
+						) : (
+							<a className={s.disabled}>
+								Erst Auswahl treffen
+								<svg
+									width='15'
+									height='15'
+									viewBox='0 0 15 15'
+									fill='none'
+									xmlns='http://www.w3.org/2000/svg'
+								>
+									<path
+										d='M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z'
+										fill='currentColor'
+										fillRule='evenodd'
+										clipRule='evenodd'
+									></path>
+								</svg>
+							</a>
+						)
+					) : null}
+				</div>
 			</main>
 		</div>
 	);
 };
 
+function findNextNode(
+	nodes: ProcessedTaskNodes,
+	node: string,
+	selectedPath: number
+): ProcessedTaskNode {
+	let currNode = nodes[nodes[node].next[selectedPath]];
+
+	while (currNode.type == "empty") {
+		currNode = nodes[currNode.next[0]];
+	}
+	return currNode;
+}
 export default Gruendung;
