09b26ac20d157d39c6a1cb14f520211466566a28
Konstantin Spieß <konstantinspiess@outlook.de>
2022-02-04 00:31:09 +0100

    redirect if no company

--- apps/frontend/src/pages/Gruendung/$task.tsx
+++ apps/frontend/src/pages/Gruendung/$task.tsx
@@ -7,7 +7,7 @@ import {
 	useGruendungContext,
 } from "contexts/Gruendung/Gruendung";
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, Redirect, useParams } from "react-router-dom";
 import TaskList from "../../components/TaskList/TaskList";
 import s from "./Gruendung.module.scss";
 import { CompletedToggle } from "./subcomponents/CompletedToggle/CompletedToggle";
@@ -20,6 +20,10 @@ const Gruendung_TaskId = () => {
 		undefined | "loading" | "error"
 	>();
 	let { task } = useParams<{ task: string }>();
+
+	const redirectIfNoCompany = !currentCompany && (
+		<Redirect to='/gruendung'></Redirect>
+	);
 
 	async function setMarkDownComponent() {
 		setLoadingState("loading");
@@ -69,136 +73,141 @@ const Gruendung_TaskId = () => {
 	}, [task, currentCompany]);
 
 	return (
-		<div className={s.splitView}>
-			<div className={s.nav}>
-				<TaskList />
-			</div>
+		<>
+			{redirectIfNoCompany}
+			<div className={s.splitView}>
+				<div className={s.nav}>
+					<TaskList />
+				</div>
 
-			<main className='content'>
-				<h1 className={s.header}>{unprocessedNodes[task].name}</h1>
-				{unprocessedNodes[task].type == "task" && (
-					<CompletedToggle taskId={task} />
-				)}
+				<main className='content'>
+					<h1 className={s.header}>{unprocessedNodes[task].name}</h1>
+					{unprocessedNodes[task].type == "task" && (
+						<CompletedToggle taskId={task} />
+					)}
 
-				{loadingState == "loading" && (
-					<div className={s.loadingIndicator}>
-						<div></div>
-						<div></div>
-						<div></div>
-						<div></div>
-					</div>
-				)}
-
-				{loadingState == "error" && (
-					<div>
-						<p>Fehler beim Laden der Seite...</p>
-						<div onClick={() => location.reload()}>
-							<Button>Seite neu Laden</Button>
-						</div>
-					</div>
-				)}
-
-				{htmlContent && (
-					<div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-				)}
-
-				{/* Show decision options for decision */}
-				{task &&
-					unprocessedNodes[task] &&
-					unprocessedNodes[task].type == "decision" && (
-						<div className={s.aside}>
-							<TaskNodeContainer
-								checked={nodes[task]?.selectedPath == 0}
-								onChange={active =>
-									setDecisionStatus(
-										task,
-										nodes[task]?.selectedPath == 0 ? undefined : 0
-									)
-								}
-								onClick={() => {
-									setDecisionStatus(
-										task,
-										nodes[task]?.selectedPath == 0 ? undefined : 0
-									);
-								}}
-								text={
-									nodes[task]?.selectedPath == 0
-										? unprocessedNodes[unprocessedNodes[task].next[0]]
-												.shortName + " gewählt"
-										: unprocessedNodes[unprocessedNodes[task].next[0]]
-												.shortName + " wählen"
-								}
-							/>
-							<TaskNodeContainer
-								checked={nodes[task]?.selectedPath == 1}
-								onChange={active => {
-									console.log(active);
-									setDecisionStatus(
-										task,
-										nodes[task]?.selectedPath == 1 ? undefined : 1
-									);
-								}}
-								onClick={() => {
-									setDecisionStatus(
-										task,
-										nodes[task]?.selectedPath == 1 ? undefined : 1
-									);
-								}}
-								text={
-									nodes[task]?.selectedPath == 1
-										? unprocessedNodes[unprocessedNodes[task].next[1]]
-												.shortName + " gewählt"
-										: unprocessedNodes[unprocessedNodes[task].next[1]]
-												.shortName + " wählen"
-								}
-							/>
+					{loadingState == "loading" && (
+						<div className={s.loadingIndicator}>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
 						</div>
 					)}
 
-				{/* back / forward navigation */}
-				<div className={s.bottomNav}>
-					{/* Go back */}
-					{nodes[task]?.prev[0] && (
-						<Link to={nodes[task].prev[0]} key={nodes[task].prev[0]}>
-							<ArrowSmLeftIcon />
-							Zurück zu {nodes[nodes[task].prev[0]].shortName}
-						</Link>
+					{loadingState == "error" && (
+						<div>
+							<p>Fehler beim Laden der Seite...</p>
+							<div onClick={() => location.reload()}>
+								<Button>Seite neu Laden</Button>
+							</div>
+						</div>
 					)}
 
-					{/* if not decision, go forward */}
-					{nodes[task]?.type != "decision" && nodes[task]?.next[0] && (
-						<Link
-							to={unprocessedNodes[task].next[0]}
-							key={unprocessedNodes[task].next[0]}
-						>
-							Weiter zu{" "}
-							{unprocessedNodes[unprocessedNodes[task].next[0]].shortName}
-							<ArrowSmRightIcon />
-						</Link>
+					{htmlContent && (
+						<div dangerouslySetInnerHTML={{ __html: htmlContent }} />
 					)}
 
-					{/* if decision && selctedPath ? next : selectsomething */}
-					{nodes[task]?.type == "decision" ? (
-						nodes[task]?.selectedPath != undefined ? (
-							<Link to={unprocessedNodes[task].next[nodes[task].selectedPath!]}>
+					{/* Show decision options for decision */}
+					{task &&
+						unprocessedNodes[task] &&
+						unprocessedNodes[task].type == "decision" && (
+							<div className={s.aside}>
+								<TaskNodeContainer
+									checked={nodes[task]?.selectedPath == 0}
+									onChange={active =>
+										setDecisionStatus(
+											task,
+											nodes[task]?.selectedPath == 0 ? undefined : 0
+										)
+									}
+									onClick={() => {
+										setDecisionStatus(
+											task,
+											nodes[task]?.selectedPath == 0 ? undefined : 0
+										);
+									}}
+									text={
+										nodes[task]?.selectedPath == 0
+											? unprocessedNodes[unprocessedNodes[task].next[0]]
+													.shortName + " gewählt"
+											: unprocessedNodes[unprocessedNodes[task].next[0]]
+													.shortName + " wählen"
+									}
+								/>
+								<TaskNodeContainer
+									checked={nodes[task]?.selectedPath == 1}
+									onChange={active => {
+										console.log(active);
+										setDecisionStatus(
+											task,
+											nodes[task]?.selectedPath == 1 ? undefined : 1
+										);
+									}}
+									onClick={() => {
+										setDecisionStatus(
+											task,
+											nodes[task]?.selectedPath == 1 ? undefined : 1
+										);
+									}}
+									text={
+										nodes[task]?.selectedPath == 1
+											? unprocessedNodes[unprocessedNodes[task].next[1]]
+													.shortName + " gewählt"
+											: unprocessedNodes[unprocessedNodes[task].next[1]]
+													.shortName + " wählen"
+									}
+								/>
+							</div>
+						)}
+
+					{/* back / forward navigation */}
+					<div className={s.bottomNav}>
+						{/* Go back */}
+						{nodes[task]?.prev[0] && (
+							<Link to={nodes[task].prev[0]} key={nodes[task].prev[0]}>
+								<ArrowSmLeftIcon />
+								Zurück zu {nodes[nodes[task].prev[0]].shortName}
+							</Link>
+						)}
+
+						{/* if not decision, go forward */}
+						{nodes[task]?.type != "decision" && nodes[task]?.next[0] && (
+							<Link
+								to={unprocessedNodes[task].next[0]}
+								key={unprocessedNodes[task].next[0]}
+							>
 								Weiter zu{" "}
-								{
-									unprocessedNodes[
-										unprocessedNodes[task].next[nodes[task].selectedPath!]
-									]?.shortName
-								}
+								{unprocessedNodes[unprocessedNodes[task].next[0]].shortName}
 								<ArrowSmRightIcon />
 							</Link>
-						) : (
-							<a className={s.disabled}>
-								Erst Auswahl treffen
-								<ArrowSmRightIcon />
-							</a>
-						)
-					) : null}
-				</div>
-			</main>
-		</div>
+						)}
+
+						{/* if decision && selctedPath ? next : selectsomething */}
+						{nodes[task]?.type == "decision" ? (
+							nodes[task]?.selectedPath != undefined ? (
+								<Link
+									to={unprocessedNodes[task].next[nodes[task].selectedPath!]}
+								>
+									Weiter zu{" "}
+									{
+										unprocessedNodes[
+											unprocessedNodes[task].next[nodes[task].selectedPath!]
+										]?.shortName
+									}
+									<ArrowSmRightIcon />
+								</Link>
+							) : (
+								<a className={s.disabled}>
+									Erst Auswahl treffen
+									<ArrowSmRightIcon />
+								</a>
+							)
+						) : null}
+					</div>
+				</main>
+			</div>
+		</>
 	);
 };
 
