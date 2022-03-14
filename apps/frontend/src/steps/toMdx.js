02946f6474ada92d4a8452e9487c767fbbcc7e8b
Malte <hi@malts.me>
2022-03-14 19:02:49 +0100

    convert every md file to mdx

--- apps/frontend/src/steps/toMdx.js
+++ apps/frontend/src/steps/toMdx.js
@@ -1,9 +1,15 @@
-var fs = require('fs');
+import fs from "fs"
+
 var files = fs.readdirSync('./');
 
-files.forEach(fileName => {
-  let file = fs.readFileSync(fileName);
-  let 
+files.filter(filename => filename.endsWith(".md")).forEach(filename => {
+  let file = fs.readFileSync(filename).toString();
+  
+  file = file.replaceAll(/\$gmbh\{(.*?)\}/gms, "<Hide showIf={props.legalForm == \"gmbh\"}> $1 </Hide>");
+  file = file.replaceAll(/\$ug\{(.*?)\}/gms, "<Hide showIf={props.legalForm == \"ug\"}> $1 </Hide>");
+
+  fs.writeFileSync(
+    filename.split(".")[0] + ".mdx",  "import Hide from \"./Hide\"\n\n" + file)
 })
 
-fs.writeFile()
+// fs.writeFile()
